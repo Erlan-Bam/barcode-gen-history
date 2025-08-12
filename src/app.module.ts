345e2d91d4ef5b0f54022ae442ee54988aa4f988
Erlan-Bam <erlanzh.gg@gmail.com>
2025-08-12 16:12:49 +0500

    fix: app is now running

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BarcodeModule } from './barcode/barcode.module';
 import { AdminModule } from './admin/admin.module';
+import { SharedModule } from './shared/shared.module';
 
 @Module({
-  imports: [BarcodeModule, AdminModule],
+  imports: [BarcodeModule, AdminModule, SharedModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/barcode/barcode.module.ts
+++ src/barcode/barcode.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { BarcodeController } from './barcode.controller';
 import { BarcodeService } from './barcode.service';
+import { SharedModule } from 'src/shared/shared.module';
 
 @Module({
+  imports: [SharedModule],
   controllers: [BarcodeController],
-  providers: [BarcodeService]
+  providers: [BarcodeService],
 })
 export class BarcodeModule {}
